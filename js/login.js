179f5f15083917d7b44583119bd7e2a70ff7c83c
Andre Ferreira <aferreira1535@gmail.com>
2022-06-22 16:49:07 -0300

    login

---
+++ js/login.js
@@ -0,0 +1,28 @@
+function logar() {
+    var username = document.getElementById('username').value
+    var password = document.getElementById('password').value
+
+    console.log(JSON.stringify({ 
+        username:username,
+        password:password
+    }));
+
+    fetch("login", (
+        method:'POST',
+        body: JSON.stringify({
+            username:username, 
+            password:password
+        }),
+        headers: {"content-type" : "aplication/json"}
+    })
+
+    then(async (resp) => {
+        var status = await resp.text();
+        console.log(status);
+        if(status == 'conectado'){
+            location.href = '/acesso-restrito/acesso.html'
+        }else{
+            alert('Nome ou senha inválidos !!')
+        }
+    })
+}
