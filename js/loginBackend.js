ad432fa6474387d51be78d2e5ccf2510c685c089
Andre Ferreira <aferreira1535@gmail.com>
2022-06-23 17:40:53 -0300

    teste

--- js/loginBackend.js
+++ js/loginBackend.js
@@ -1,57 +1,68 @@
 const http = require('http')
 const path = require('path')
-const express = require('express')
+
+const express =  require('express')
 const fs = require("fs");
-var session = require ('express-session')
+var session = require('express-session')
+
+
 const app = express()
 const server = http.createServer(app)
+
+
+
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(session({secret:"abc"}));
 
-//configurações
+   // configuraçoes
+   app.set('port', process.env.PORT || 5500)
 
-app.set('port', process.env.PORT || 5500)
 
-// Secção de login
 
-app.use('/indexHTML/acesso.html/*', (req, res, next) => {
-    if(req.session.nome){
+
+// secção de login
+app.use('/indexHTML/acesso/*', (req, res, next) => {
+    if( req.session.nome ){
         next();
-    } else {
-        res.redirect('./indexHTML/acesso.html')
+    }else{
+        res.redirect('/indexHTML/loginfe.html')
     }
-});
+      });
 
 
+      // artigos estaticos
 app.use(express.static(path.join(__dirname, 'public')))
 
 //start do server
 server.listen(app.get('port'), () => {
     console.log('server na porta', app.get('port'))
+   
+   
 })
 
 
+// secção de login 2
 
-// login do front para o back
 
-app.post('/indexHTML/loginfe.html', (req,res) =>{
-    const usuarioscad = fs.readFileSync('./js/usuarios.json')
+app.post('/login',(req, res) => {
+    const usuarioscad =   fs.readFileSync('./js/usuarios.json')
     const usuariosparse = JSON.parse(usuarioscad)
+    
 
     var nome = req.body.nomes
     var senha = req.body.senha
 
-    for(var umUsuario of usuariosparse) {
-        if (nome == umUsuario.nome && senha == umUsuario.senha) {
-            req.session.nome = umUsuario
-            res.send('conectado')
-            return
+
+        for( var umUsuario of usuariosparse) {
+            if(nome == umUsuario.nome && senha == umUsuario.senha ){
+                    req.session.nome = umUsuario
+                    res.send('conectado')
+                    return
+            }
+                
+            
         }
-    }
-    res.send('falhou')
+        res.send('falhou')
+    
 })
-
-
-
-
