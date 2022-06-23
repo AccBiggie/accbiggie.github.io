67d9386d08e9c53c1acac9c82ab27bf2b7be072d
Andre Ferreira <aferreira1535@gmail.com>
2022-06-23 17:32:05 -0300

    alteração

--- js/login.js
+++ js/login.js
@@ -1,27 +1,31 @@
-function logar() {
-    var username = document.getElementById('#username').value
-    var password = document.getElementById('#password').value
+function logar(){
+    var nomes = document.getElementById('#username').value
+    var senha = document.getElementById('#paddword').value
 
-    console.log(JSON.stringify({ 
-        username:username,
-        password:password
-    }));
+console.log(JSON.stringify({
+    nomes:nomes,
+    senha:senha
+}));
 
-    fetch("login"),
-        method,'POST'
-        body, JSON.stringify({
-            username:username, 
-            password:password
-        }),
-        headers = {"content-type" : "aplication/json"}
-    }
+    fetch("/login",{
+        method:'POST',
+        body: JSON.stringify({
+            nomes:nomes,
+            senha:senha
+        }) , 
+        headers: { "Content-Type" : "application/json" }
+        
+    })
 
-    then(async (resp) => {
+    .then(async (resp) => {
         var status = await resp.text();
-        console.log(status);
-        if(status == 'conectado'){
-            location.href = "/acesso-restrito/acesso.html"
-        }else{
-            alert('Nome ou senha inválidos !!')
+        console.log(status)
+        if(status == 'conectado' ){
+            location.href = '/indexHTML/acesso.html'
+        }else {
+            alert('nome e senha invalidos!!')
         }
+        
     });
+
+}
