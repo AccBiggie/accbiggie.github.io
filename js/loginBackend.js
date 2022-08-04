13bd96cf6cfe7d6eb78db795ca9115f2cf62bed0
Andre Ferreira <aferreira1535@gmail.com>
2022-08-04 18:03:16 -0300

    imp ecommerce

--- js/loginBackend.js
+++ js/loginBackend.js
@@ -1,15 +1,10 @@
 const http = require('http');
 const path = require('path');
-
 const express =  require('express');
 const fs = require("fs");
 var session = require('express-session');
-
-
 const app = express();
 const server = http.createServer(app);
-
-
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -17,9 +12,6 @@ app.use(session({secret:"abc"}));
 
    // configuraçoes
    app.set('port', process.env.PORT || 5500);
-
-
-
 
 // secção de login
 app.use("./indexHTML/acesso/", (req, res, next) => {
@@ -30,38 +22,29 @@ app.use("./indexHTML/acesso/", (req, res, next) => {
     }
       });
 
-
       // artigos estaticos
 app.use(express.static(path.join(__dirname, 'public')))
 
 //start do server
 server.listen(app.get('port'), () => {
     console.log('server na porta', app.get('port'))
-   
-   
+      
 })
 
-
 // secção de login 2
-
-
 app.post('/Entrar',(req, res) => {
     const usuarioscad =   fs.readFileSync("/js/usuarios.json")
     const usuariosparse = JSON.parse(usuarioscad)
     
-
     var nome = req.body.nomes
     var senha = req.body.senha
-
-
+    
         for( var umUsuario of usuariosparse) {
             if(nome == umUsuario.nome && senha == umUsuario.senha ){
                     req.session.nome = umUsuario
                     res.send('conectado')
                     return
-            }
-                
-            
+            }         
         }
         res.send('falhou')
     
