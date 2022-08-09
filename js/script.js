d4bff008bb8d5b81506b9d09ef4fcdaf27757637
Andre Ferreira <aferreira1535@gmail.com>
2022-08-09 13:55:12 -0300

    imp eccomerce

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@
 import Countdown from "./countdown.js";
 
 const tempoLoja = new Countdown ("15 August 2022 20:00:00 GMT-0300");
-const tempos = document.querySelectorAll("[data-time]");
+const firstTime = document.querySelectorAll("[data-time]");
 
 function mostrarTempo() {
-tempos.forEach((tempo, index) => {
-    tempo.innerHTML = tempoLoja.total[index];
-});
+    firstTime.forEach((tempo, index) => {
+        tempo.innerHTML = tempoLoja.total[index];
+    });
 }
+
 mostrarTempo();
 setInterval(mostrarTempo, 1000);
