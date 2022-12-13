c2da7324ae0ce64e09a4a9007bfc606fd49f2167
Andre Ferreira da Silva <andre.ferreira@infinitsolucoes.com.br>
2022-12-12 22:58:49 -0300

    Alterações Projeto

--- ecommerce/backend/middleware/catchAsyncErrors.js
+++
@@ -1,4 +0,0 @@
-//Backend Error Handling
-module.exports = theFunc => (req,res,next) => {
-    Promise.resolve(theFunc(req,res,next)).catch(next);
-};
--- ecommerce/backend/utils/errorHander.js
+++
@@ -1,9 +0,0 @@
-//Backend Error Handling
-class ErrorHander extends Error {
-    constructor(message,statusCode) {
-        super(message);
-        this.statusCode = statusCode;
-        Error.captureStackTrace(this,this.constructor);
-    }
-}
-module.exports = ErrorHander;
